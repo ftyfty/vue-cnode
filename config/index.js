171d1379349798464e8063d6fe4d2b6891f84696
fty <1046041114@qq.com>
2017-05-07 15:17:26 +0800

    页面

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../dis/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dis'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
     productionSourceMap: true,
